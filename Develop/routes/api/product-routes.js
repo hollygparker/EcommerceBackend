9b2f0c46166237126888ac89e487bed4fba51509
Holly Parker <hollygcochran@gmail.com>
2022-09-07 22:12:58 -0500

    creating product routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
+const { restore } = require('../../models/Product');
 
 // The `/api/products` endpoint
 
@@ -7,12 +8,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, {
+      model: Tag,
+      through: ProductTag
+    },
+  ],
+  })
+  .then ((products) => res.json(products))
+  .catch ((error) => res.status(500).json(error))
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Category, {
+      model: Tag,
+      through: ProductTag
+    },
+  ],
+  })
 });
 
 // create new product
